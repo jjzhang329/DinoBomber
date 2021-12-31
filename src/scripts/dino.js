9c9bbd7b879c37afdea9064b79fa3de7af90a1e7
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2021-12-31 01:10:37 -0600

    drawSprite

--- src/scripts/dino.js
+++ src/scripts/dino.js
@@ -2,18 +2,20 @@ class Dino{
     constructor(ctx){
         this.x = 0;
         this.y = 460;
-        this.width = 40;
-        this.height = 40;
+        this.width = 32;
+        this.height = 32;
         this.move = 1
         this.draw(ctx)
 
     }
 
     draw(ctx){
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        // ctx.fillStyle = 'green';
+        // ctx.fillRect(this.x, this.y, this.width, this.height)
         const dinoSprite = new Image();
-        // dinoSprite.src = 
+        dinoSprite.src = "../assets/DinoSheet.png";
+        ctx.drawImage(dinoSprite, 0, 0, this.width, 
+            this.height, 0, 460, this.width, this.height)
     }
 
    move(){
