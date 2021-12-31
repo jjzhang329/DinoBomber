38827368866ad8742f43d8dd56eb98d2eb7192d5
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2021-12-31 12:55:47 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 const Dino = require('./scripts/dino.js')
 
 
-const keys = [];
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
-ctx.clearRect(0, 0, canvas.width, canvas.height)
+
 const newDino = new Dino(ctx)
 
 //if this game clieckd, start game;
 //if this cliecked, restart game;
 //
 
-window.addEventListener('keydown', function (e) {
-    keys[e.keycode] = true;
-})
-window.addEventListener('keyup', function (e) {
-    delete keys[e.keycode];
-})
+function animate() {
+   
+  
+}
+animate();
--- src/scripts/dino.js
+++ src/scripts/dino.js
@@ -1,31 +1,56 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const dinoSprite = new Image();
+dinoSprite.src = "src/assets/DinoSheet.png";
+canvas.width = 800;
+canvas.height = 500;
+
 class Dino{
     constructor(ctx){
         this.x = 0;
         this.y = 460;
-        this.width = 32;
-        this.height = 32;
-        this.move = 1
-        this.draw(ctx)
+        this.width = 64;
+        this.height = 64;
+        this.frameX = canvas.width - this.width;
+        this.frameY = canvas.height - this.height;
+        this.move = 10;
+       this.draw(ctx)
+        
+    }
+
+    draw(ctx) {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        dinoSprite.onload = () => {
+            ctx.drawImage(dinoSprite, 64, 0, 32,
+                32, this.frameX, this.frameY, this.width, this.height)
+        }
 
     }
 
-    draw(ctx){
-        // ctx.fillStyle = 'green';
-        // ctx.fillRect(this.x, this.y, this.width, this.height)
-        const dinoSprite = new Image();
-        dinoSprite.src = "../assets/DinoSheet.png";
-        ctx.drawImage(dinoSprite, 0, 0, this.width, 
-            this.height, 0, 460, this.width, this.height)
-    }
+  
 
-   move(){
-       //if up key is pressed
-       if(keys[38]){
-           this.y -= this.move;
-       }
-   }
+    
+
+    
+
+//    move(){
+//        //if up key is pressed
+//        if(keys[38]){
+//            this.y -= this.move;
+//        }
+//    }
 
 }
+const keys = [];
+window.addEventListener('keydown', function (e) {
+    keys[e.keycode] = true;
+    console.log(e.keycode)
+})
+window.addEventListener('keyup', function (e) {
+    delete keys[e.keycode];
+})
+
 
 
 
