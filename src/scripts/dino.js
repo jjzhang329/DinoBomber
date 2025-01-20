0ef87a927f2d655f2f0df059183b8e0d367e122f
Daniel Ramirez <coffee-dan@protonmail.com>
2025-01-19 22:56:31 -0600

    refactor: add keymap to allow multiple dinos

--- src/scripts/dino.js
+++ src/scripts/dino.js
@@ -1,11 +1,39 @@
 import Bomb from './bomb';
 import MovingObjects from './movingObjects';
 
+const STD_KEYMAP = {
+    "up": "ArrowUp",
+    "left": "ArrowLeft",
+    "right": "ArrowRight",
+    "down": "ArrowDown",
+    "action": "Space"
+}
+
+const ALT_KEYMAP = {
+    "up": "KeyW",
+    "left": "KeyA",
+    "right": "KeyD",
+    "down": "KeyS",
+    "action": "KeyE"
+}
+
+class KeyMap {
+    constructor(...kwargs) {
+        kwargs = {...STD_KEYMAP, ...kwargs}
+        this.up     = kwargs["up"]
+        this.left   = kwargs["left"]
+        this.right  = kwargs["right"]
+        this.down   = kwargs["down"]
+        this.action = kwargs["action"]
+    }
+}
+
 export default class Dino extends MovingObjects {
     constructor(object) {
-        super(object)
+        super(object);
         this.width = 24;
         this.height = 28;
+        this.keyMap = new KeyMap();
         this.bomb = 0;
         this.game = object.game;
         this.newBomb = [];
@@ -33,28 +61,28 @@ export default class Dino extends MovingObjects {
 
     move(key) {
         let moving = false
-        if (key['ArrowUp'] && this.canMoveUp()) {
+        if (key[this.keyMap.up] && this.canMoveUp()) {
             this.y -= this.speed;
             this.width = 21;
             this.height = 29;
             this.frameY = 2.93;
             moving = true;
         }
-        if (key['ArrowLeft'] && this.canMoveLeft()) {
+        if (key[this.keyMap.left] && this.canMoveLeft()) {
             this.x -= this.speed;
             this.width = 24;
             this.height = 28;
             this.frameY = 0;
             moving = true;
         }
-        if (key['ArrowRight'] && this.canMoveRight()) {
+        if (key[this.keyMap.right] && this.canMoveRight()) {
             this.x += this.speed;
             this.width = 24;
             this.height = 28;
             this.frameY = 1;
             moving = true;
         }
-        if (key['ArrowDown'] && this.canMoveDown()) {
+        if (key[this.keyMap.down] && this.canMoveDown()) {
             this.y += this.speed
             this.width = 21;
             this.height = 29;
@@ -64,7 +92,7 @@ export default class Dino extends MovingObjects {
 
         this.moving = moving
 
-        if (key['Space'] && this.emptyTile(this.x, this.y)) {
+        if (key[this.keyMap.action] && this.emptyTile(this.x, this.y)) {
             this.newBomb.push(new Bomb(this.x, this.y, this.game));
             this.bomb += 1;
         }
